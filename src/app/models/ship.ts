20a0ffde83432a1de3228232df6a7baddf89bf2d
xcgpseud <xcgpseud@github.com>
2018-04-01 22:21:32 +0100

    services changes

--- src/app/models/ship.ts
+++ src/app/models/ship.ts
@@ -10,4 +10,22 @@ export class Ship{
         this.end = end;
     }
 
+    getPoints(): Array<Point> {
+        let points = Array<Point>();
+    
+        // If ship is horizontal
+        if (this.start.x < this.end.x) {
+          var y = this.start.y;
+          for (let x = this.start.x; x <= this.end.x; x++) {
+            points.push(new Point(0, x, y));
+          }
+        } else {
+          var x = this.start.x;
+          for (let y = this.start.y; y <= this.end.y; y++) {
+            points.push(new Point(0, x, y));
+          }
+        }
+        return points;
+      }
+
 }
--- src/app/services/ship.service.ts
+++ src/app/services/ship.service.ts
@@ -7,23 +7,60 @@ import { Point } from '../models/point';
 export class ShipService {
 
   board: Board;
+  width: number;
+  height: number;
 
-  constructor(
-    width: number,
-    height: number
-  ) {
-    // todo: Set these params properly
-    this.board = new Board(width, height);
+  constructor(width: number, height: number) {
+    this.width = width;
+    this.height = height;
+  }
+
+  generatePossibleShips(length: number){
+    this.board = new Board(this.width, this.height);
+    this.fillBoard(length);
+
+  }
+
+  getNonIntersectingShips(board: Board): Array<Ship>{
+    let nonIntersectingShips = Array<Ship>();
+    board.ships.forEach(ship => {
+      let intersects = false;
+      this.board.ships.forEach(currentShip => {
+        intersects = this.shipsIntersect(ship, currentShip);
+        if(intersects){
+          return;
+        }
+      });
+      if(!intersects){
+        nonIntersectingShips.push(ship);
+      }
+    })
+  }
+
+  shipsIntersect(ship: Ship, otherShip: Ship): boolean {
+
+    let intersects = false;
+
+    ship.getPoints().forEach(s => {
+      otherShip.getPoints().forEach(o => {
+        if (s.x == o.x && s.y == o.y) {
+          intersects = true;
+        }
+      })
+    });
+
+    return intersects;
+
   }
 
   // todo: intersects, remove intersecting, return free ship on given board
 
-  fillBoard(length: number): void {
+  private fillBoard(length: number): void {
     this.fillHorizontal(length);
     this.fillVertical(length);
   }
 
-  fillHorizontal(length: number): void {
+  private fillHorizontal(length: number): void {
     for (let x = 1; x <= this.board.width - length; x++) {
       for (let y = 1; y <= this.board.height; y++) {
         this.board.ships.push(new Ship(
@@ -34,7 +71,7 @@ export class ShipService {
     }
   }
 
-  fillVertical(length: number): void {
+  private fillVertical(length: number): void {
     for (let x = 1; x <= this.board.width; x++) {
       for (let y = 1; y <= this.board.height - length; y++) {
         this.board.ships.push(new Ship(
